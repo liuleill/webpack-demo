fdd1acdd583a1624ee6a036337ed4507560a0990
刘磊 <2413882532@qq.com>
2021-05-06 19:56:12 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const base = require('./webpack.config.base.js')
 
 module.exports = {
-  mode: 'development',
-  devtool:'inline-source-map',
-  devServer: {
-    contentBase: './dist',
+  ...base,
+  devtool:"inline-source-map",
+  devServer:{
+    contentBase:"./dist"
   },
-  entry: './src/index.js',
-  output: {
-    filename: '[name].[contenthash].js',
-  },
-  plugins: [new HtmlWebpackPlugin({
-      title:'liulei',
-      template:'src/assets/index.html'
-  })],
-
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
 
